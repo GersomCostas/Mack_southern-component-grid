719d4fce959a315322c0165d952750f8e09d7e34
Gersom Costas <gersom.costas@vi.ieo.es>
2017-11-02 14:07:08 +0100

    update

--- R/gridding_MAC_Southern.R
+++ R/gridding_MAC_Southern.R
@@ -1,45 +1,42 @@
-################################################################################
-################################################################################
+###################################
 ## SCRIPT : ANNUAL EGG PRODUCTION FOR NEA MACKEREL. GRIDDING WESTERN AREA 
-## version SEPTEMBER 2017
+## version October 2017
 ##
-## Gersom Costas (Instituto Espa?ol de Oceanografia)
+## Gersom Costas 
 ## gersom.costas@ieo.es
-################################################################################
-################################################################################
-##DESCRIPTION 
-# The  mackerel and western horse mackerel egg surveys (WGMEGG) cover spawning
-# areas for NEA mackerel and Western horse mackerel stocks. 
-#The north-east Atlantic shelf area is sub-divided  into two spawning areas: 
-#western and southern areas.  The southern area for NEA mackerel is regarded as 
-#being from 36? N to 44? N in the east and 45? N in the west. 
-#The western area for NEA mackerel is from 44? N (45? N in the west) to 63? N  
-#The western area for Western horse mackerel includes the Cantabrian Sea and is 
-#from 43? N to 63? N with same western boundary as for mackerel
-#The plankton survey grid was designed according to the procedure described in 
-#AEPM manual (ICES, 2016). The basic sampling unit is 0.5 degree longitude * 0.5 degree latitude,
-# half of an ICES rectangle. 
-#In the Cantabrian coast and in Gulf of Cadiz, the standard half ICES rectangle was
-#changed to a quarter degree latitude by one degree longitude because transects 
-#in those regions were done perpendicular to the 200 m depth contour line.
+#
+###################################
+#
+##DESCRIPTION
 
-################################################################################
+# The international mackerel and horse mackerel egg surveys (WGMEGS) cover spawning areas for NEA mackerel and horse mackerel stocks. The spatial and temporal distribution of sampling is designed to ensure an adequate coverage of both mackerel  and horse mackerel spawning areas. 
+#The Northeast Atlantic shelf area is subdivided into 'western' and 'southern' areas for the purposes of estimating egg production and SSB. 
+#The western area for NEA mackerel is from 44 degrees N (45 degrees N in the west) to 63 degrees N. It includes Biscay, the Celtic Sea and the shelf edge to the northwest of Scotland. 
+#The 'southern' area for mackerel is regarded as being from 36 degrees N to 44 degrees N in the east and 45 degrees N in the west . It extends from Cape Trafalgar in the Gulf of Cadiz, around the coast of Portugal to 11 degrees W, the Cantabrian Sea and southern Biscay.
+#The plankton survey grid was designed according to the procedure described in AEPM manual (ICES, 2016). The basic sampling unit is 0.5 degree longitude * 0.5 degree latitude,  half of an ICES rectangle. 
+#But in the Cantabrian coast and in Gulf of Cadiz, the standard half ICES rectangle was changed to a quarter degree latitude by one degree longitude because transects in those regions were done perpendicular to the 200 m depth contour line.
 
-##  SPATIAL GRID  OF MACKEREL SOUTHERN AREA.  CREATING GRID
+####################################################
 
-#################################################################################
-##  Southern area:
+##  SPATIAL GRID  SOUTHERN AREA.
 
-#Latitude: lower or equal 45 degree N   y bigger  or equal  36 degree N
-#Longitude: bigger or equal 1 degree W   y lower or equal  10.5 degree W
-#################################################################################
+###  Southern area:
+
+# Latitude: lower or equal 45 degree N   y bigger  or equal  36 degree N
+# Longitude: bigger or equal 1 degree W   y lower or equal  10.5 degree W
+
+####################################################
+
+
 # clear workspace
+
 rm(list = ls())
 
 
 ##################
 # load libraries
 ##################
+
 if(!require(plyr)) install.packages("plyr") ; require(plyr)
 if(!require(sp)) install.packages("sp") ; require(sp)
 if(!require(maptools)) install.packages("maptools") ; require(maptools)
@@ -48,115 +45,147 @@ if(!require(maps)) install.packages("maps") ; require(maps)
 if(!require(mapdata)) install.packages("mapdata") ; require(mapdata)
 
 
-#Importing Western Component MAC rectangles have been sampled along survey temporal series.  
-#importing grid data by region. rectangle dimensions in southern area is a combination of differnt size rectangles.
-#importing grid data by region. rectangle dimensions depende on  region
+# Importing mackerel Southern Component  rectangles have been sampled along MEGS survey time series.  
+# importing grid data by region. southern area contains different size rectangles.
+# Rectangle dimensions depende on  region: Cantabrian Sea, Iberian Atlatic coast and Gulf of Cadiz
 
 
-  RECT2 <- read.csv("data/RECT2.csv")#Cantabrian Sea, rectangles 0.25x1
-  RECT3 <- read.csv("data/RECT3.csv")#Iberian Atlatic coast rectangles 0.5x0.5
-  RECT5 <- read.csv("data/RECT5.csv")#Gulf of Cadiz, rectangles 0.25x1
-
-#  Mackerel southern area
+  RECT2 <- read.csv("data/RECT2.csv")#Cantabrian Sea, rectangles 0.25x1 degrees
   
-  RECT_south<-rbind(RECT2,RECT3,RECT5)#
-  summary(RECT_south) 
+  RECT3 <- read.csv("data/RECT3.csv")#Iberian Atlatic coast rectangles 0.5x0.5 degrees
+  
+  RECT5 <- read.csv("data/RECT5.csv")#Gulf of Cadiz, rectangles 0.25x1 degrees
 
-##Importing  file of proportion in rectangles area that correspond sea(removing covered area by land from rectangle area)
-## Rectangle area   (cos(RECT$lat*pi/180)*30*1853.2)*30*1853.2 
-#all rectangles, rectangles 0.5? x 0.5? and 0.25? x 1? (Cantrabian Sea)  
   
+# Mackerel southern area. Combining different grids in an object (data.frame)
   
+  RECT_south<-rbind(RECT2,RECT3,RECT5)
+  
+
+# Importing  file of proportion in rectangles area that correspond sea(removing covered area by land from rectangle area)
+# Rectangle area   (cos(RECT$lat*pi/180)*30*1853.2)*30*1853.2 
+
   RECTall <- read.csv("data/rect_searatio_all.csv")
 
 
+# Matching covered area by land to southern area rectangles 
 
-##Matching covered area by land to western area rectangles( step by step) 
+##general (RECT_south)
+  
+  RECT_south<-join(RECT_south[ ,c("lat","lon","cat")],RECTall[ ,c("lat","lon","r1","R2" ,"RECT","Area","sea_ratio","Area_minus_land")],by=c("lat","lon"))
+  summary(RECT_south)
 
-
-  RECT_south<-join(RECT_south[ ,c("lat","lon","cat")],RECTall[ ,c("lat","lon","r1","R2" ,"RECT","Area","sea_ratio","Area_minus_land")],by=c("lat","lon")) 
- summary(RECT_south)
- RECT_south<-RECT_south[complete.cases(RECT_south),]#1 rectagle in Gibraltar street with NA->delete
- RECT_south<-droplevels(RECT_south)
-
- RECT2<-join(RECT2[ ,c("lat","lon","cat")],RECTall[ ,c("lat","lon","r1","R2" ,"RECT","Area","sea_ratio","Area_minus_land")],by=c("lat","lon"))  
- summary(RECT2)
+  #Problem with NA to be gridded. rectagle in Gibraltar street with NA->delete
+  RECT_south<-RECT_south[complete.cases(RECT_south),]
+  RECT_south<-droplevels(RECT_south)
+ 
+ 
+##region grid (RECT2, RECT3, RECT5)
+  
+ RECT2<-join(RECT2[ ,c("lat","lon","cat")],RECTall[ ,c("lat","lon","r1","R2" ,"RECT","Area","sea_ratio","Area_minus_land")],by=c("lat","lon"))
+ 
  RECT2<-droplevels(RECT2) 
  
- RECT3<-join(RECT3[ ,c("lat","lon","cat")],RECTall[ ,c("lat","lon","r1","R2" ,"RECT","Area","sea_ratio","Area_minus_land")],by=c("lat","lon")) 
- summary(RECT3)
+ RECT3<-join(RECT3[ ,c("lat","lon","cat")],RECTall[ ,c("lat","lon","r1","R2" ,"RECT","Area","sea_ratio","Area_minus_land")],by=c("lat","lon"))
+ 
  RECT3<-droplevels(RECT3)
  
- RECT5<-join(RECT5[ ,c("lat","lon","cat")],RECTall[ ,c("lat","lon","r1","R2" ,"RECT","Area","sea_ratio","Area_minus_land")],by=c("lat","lon")) 
- summary(RECT5)
- #Problem with NA to be gridded
- RECT5<-RECT5[complete.cases(RECT5),]#1 rectagle in Gibraltar street with NA->delete
+ RECT5<-join(RECT5[ ,c("lat","lon","cat")],RECTall[ ,c("lat","lon","r1","R2" ,"RECT","Area","sea_ratio","Area_minus_land")],by=c("lat","lon"))
+ 
+ #Problem with NA to be gridded.  rectagle in Gibraltar street with NA->delete
+ RECT5<-RECT5[complete.cases(RECT5),]
  RECT5<-droplevels(RECT5)
- ##################
+
  
- 
- 
-##To Spatial pixel
+# Convert data frame to Spatial pixel
  
  gridded(RECT2) = ~lon+lat
  gridded(RECT5) = ~lon+lat
  gridded(RECT3) = ~lon+lat
  
-##transform to Spatial Polygon
+ 
+# Convert spatial pixel to Spatial Polygon
  
  RECT2_p <- as(RECT2, "SpatialPolygons")
  RECT3_p <- as(RECT3, "SpatialPolygons") 
  RECT5_p <- as(RECT5, "SpatialPolygons") 
  
-#PLOTING 
+ 
+#Ploting grid 
  
  plot(RECT2_p)
  plot(RECT3_p)
  plot(RECT5_p)
  
- slotNames(RECT2_p)# # slot names
-  
-# Original Rectangle names. 
+
+# Automatic nectangle names (ID) 
+ 
  row.names(RECT2_p) 
  row.names(RECT3_p) 
  row.names(RECT5_p) 
  
-## Use the spChFIDs() method in maptools to change the  IDs of at least one of your objects to disambiguate them 
+ 
+# Use the spChFIDs() method in maptools to change the  IDs of at least one of your objects to disambiguate them 
  
  RECT2_p <- spChFIDs(RECT2_p, as.character(RECT2@data[, 4]))
+ 
  RECT3_p <- spChFIDs(RECT3_p, as.character(RECT3@data[, 4]))
+ 
  RECT5_p <- spChFIDs(RECT5_p, as.character(RECT5@data[, 4]))
  
  
-# join spatialpoligonos ( step by step).
+# joining region spatialpolygonos ( step by step).
  
  rownames(RECT_south)<-RECT_south$RECT
  
- RECT_p<-spRbind(RECT2_p,RECT3_p)
- RECT_p<-spRbind(RECT_p,RECT5_p)
+ RECTsouth_p<-spRbind(RECT2_p,RECT3_p)
  
- ## Projection 
- proj4string(RECT_p) <- CRS("+proj=longlat   +ellps=WGS84 +datum=WGS84")
- RECT_p<-SpatialPolygonsDataFrame(RECT_p, data=RECT_south)
+ RECTsouth_p<-spRbind(RECTsouth_p,RECT5_p)
  
- plot(RECT_p)
- class(RECT_p)
- summary(RECT_p)
  
- save.image("AEPM_grid_mack_southern.RData")  
+# Projection grid
+ 
+ proj4string(RECTsouth_p) <- CRS("+proj=longlat   +ellps=WGS84 +datum=WGS84")
 
+ 
+# Convert to SpatialPolygonsDataFrame. Data frame represents rectangle characteristics (polygons)
+ 
+ RECTsouth_p<-SpatialPolygonsDataFrame(RECTsouth_p, data=RECT_south)
+ 
+
+# ploting
+
+ png("images/southern_survey_grid.png",
+     width = 7, height = 7, units = "in", pointsize = 10,
+     bg = "white", res = 800,
+     type = "cairo-png")
+ 
+ par(mar=c(2,2,2,2) + 0.1)
+ 
+ map(database = "worldHires",  xlim = c(-11,1), ylim = c(36,46),fill=T, type="n")
+ 
+ plot(RECTsouth_p, border="grey",  xlim = c(-11,1), ylim = c(36,46))
+ 
+ degAxis(2, at = c(seq(36,46, by=3)),cex.axis = 0.5,las=2)
+ 
+ degAxis(1, at = c(seq(-11,1, by=3)), cex.axis = 0.5, las=2)
+ 
+ map(database = "worldHires", xlim = c(-11,1), ylim = c(36,46),fill=T, col="darkgreen",add=T)
+ 
+ title("Mackerel southern area grid")
+ 
+ box()
+ 
+ dev.off()
+
+ 
  
   
-### import land and ploting
-
-par(mfrow=c(1,1))
-europe<-map(database = "worldHires",  xlim = c(-11,0), ylim = c(36,46),fill=T,plot=F)
-europe$names
-IDs <- sapply(strsplit(europe$names, ":"), function(x) x[1])
-europemap <- map2SpatialPolygons(europe,IDs=IDs, proj4string=CRS("+proj=longlat   +ellps=WGS84 +datum=WGS84"))
-proj4string(RECT_p) <- CRS("+proj=longlat   +ellps=WGS84 +datum=WGS84")
-plot(RECT_p, border="grey")
-plot(europemap,add=T, fill=T,col="yellow", border="grey")
+ rm(RECT2, RECT2_p,    RECT3,      RECT3_p,    RECT5,      RECT5_p,    RECTall)
+ 
+ 
+ save.image("AEPM_grid_mack_southern.RData")  
+ 
 
 
 
